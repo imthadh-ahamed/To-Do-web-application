47ab00578bff828e59205338d1218977202139e8
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-05-28 18:45:05 +0530

    Create the components

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
-const url = `mongodb+srv://pranab90:pranab90783@cluster0.0opmf0k.mongodb.net/`;
+const url = `mongodb+srv://it22077288:IT22077288@cluster0.fuzmzrf.mongodb.net/bloodbank?retryWrites=true&w=majority`;
 
 const connectDatabase = async (req, res) => {
     try {
         await mongoose
             .connect(url)
             .then(() => {
-                console.log("Database Connected!!");
+                console.log("MongoDB Connected!!");
             });
     } catch (error) {
-        console.error("Database Connection Failed");
+        console.error("MongoDB Connection Failed");
     }
 };
 connectDatabase();
